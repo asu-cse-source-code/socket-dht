15971d24982fbda38fb91b60c1cfb390fffe3db1
abspen1 <abspencer2097@gmail.com>
2021-09-23 13:44:11 -0700

    working now on base ip

--- UDPEchoServer.py
+++ UDPEchoServer.py
@@ -3,7 +3,7 @@ import sys
 import time
 
 
-serv_IP = '127.0.0.1'  # Standard loopback interface address (localhost)
+# serv_IP = socket.AF_INET  # Standard loopback interface address (localhost)
 
 
 def die_with_error(error_message):
@@ -24,7 +24,7 @@ def main(args):
     print(f"server: Port server is listening to is: {echo_serv_port}\n")
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((serv_IP, echo_serv_port))
+        s.bind(("", echo_serv_port))
         s.listen()
         conn, addr = s.accept()
         with conn:
