b0e43efcd3845929e13904436316bbfe4e30423f
abspen1 <abspencer2097@gmail.com>
2021-09-27 09:50:20 -0700

    query sped up tenfold

--- Client.py
+++ Client.py
@@ -27,13 +27,11 @@ class Client:
         self.n = None
         self.local_hash_table = [ [] for _ in range(HASH_SIZE) ]
         self.user_dht = None
-        
-    def create_sockets(self):
         # UPDServer sockets
         self.client_to_server = UDPServer()
-        self.client_left_port = UDPServer()
-        self.client_query = UDPServer()
-        self.client_right_port = UDPServer()
+        self.accept_port = UDPServer()
+        self.query_port = UDPServer()
+        self.send_port = UDPServer()
 
     def set_data(self, data):
         print('setting client data:', data)
@@ -44,6 +42,24 @@ class Client:
         self.next_node_addr = (data[1]['ip'], int(data[1]['port']))
         self.next_node_query_addr = (data[1]['ip'], int(data[1]['query']))
 
+    def check_record(self, record):
+        '''
+            Check given record and see if it is stored on the local hash table
+            If it is not set the self.record value to the record which will
+            trigger the query socket and send the query command to next node
+        '''
+        pos = hash_pos(record)
+        id = pos % self.n
+        if id == self.id:
+            # This is the desired location for record!
+            self.local_hash_table[pos].append(record)
+        else:
+            try:
+                # print(f"sending to next node addr {client.next_node_addr}")
+                self.send_port.send_response(addr=self.next_node_addr, res='SUCCESS', type='record', data=record)
+            except:
+                print("client-node: sendall() error within records connect nodes")
+
 
 def setup_all_local_dht(client):
     '''This function will read in the records one by one and call to check the record'''
@@ -52,7 +68,7 @@ def setup_all_local_dht(client):
         total_records = 0
         # Iterate over each row in the csv using reader object
         for record in csv_reader:
-            check_record(client, record)
+            client.check_record(record)
             total_records += 1
             if total_records % 20 == 0:
                 print(f"{total_records} read so far...")
@@ -67,66 +83,33 @@ def hash_pos(record):
     
     return ascii_sum % 353
 
-def check_record(client, record):
-    '''
-        Check given record and see if it is stored on the local hash table
-        If it is not set the client.record value to the record which will
-        trigger the query socket and send the query command to next node
-    '''
-    pos = hash_pos(record)
-    id = pos % client.n
-    if id == client.id:
-        # This is the desired location for record!
-        client.local_hash_table[pos].append(record)
-    else:
-        # This is not the desired location for the record
-        # print(f'The desired location is on id: {id}')
-        loops = 1
-
-        # While loop that iterates until the current record is sent to next node
-        # Could turn each of these record send commands into threads :)
-        while client.record:
-            # Sleep to allow the records to be sent
-            time.sleep(0.5)
-            # print(f'\nawaiting record to send: {loops}')
-            if loops > 4:
-                # print("\nTimeout occurred!\n\n")
-                print("The nodes lost connection with eachother while trying to transfer the records!")
-                break
-            loops += 1
-
-        client.record = record
-        # print("Sent data to next node")
-
 
 def initialize_client_topology(client):
     '''
         Connecting socket between current client and the neighboring client
         Once connection is made a new thread will be created to listen to that connection
     '''
-    client_server = UDPServer()
 
     try:
-        client_server.socket.bind(client.left_port_addr)
+        client.accept_port.socket.bind(client.left_port_addr)
     except Exception as error:
             print(error)
             print(f"server: bind() failed for client: {client.left_port_addr}")
             return
         
     print(f"client-server: Port server is listening to is: {client.left_port_addr[1]}\n")
-    start_new_thread(connect_nodes, (client_server, client))
     
     # Add loop here so that we can disconnect and reconnect to server
     while True:
         
-        message, addr = client_server.socket.recvfrom(BUFFER_SIZE)
+        message, addr = client.accept_port.socket.recvfrom(BUFFER_SIZE)
 
         # print(f"Client-server received message from addr: {addr}")
 
-        client_topology(client_server, client, message, addr)
+        client_topology(client, message, addr)
 
 
-def client_topology(client_server, client, data, addr):
+def client_topology(client, data, addr):
     '''
         Will keep the connection with neighboring client until there is a disconnect on their end
     '''
@@ -135,13 +118,13 @@ def client_topology(client_server, client, data, addr):
         data_loaded = json.loads(data_loaded)
         # print(f"client-topology: received message ``{data_loaded}''\n")
         if data_loaded['type'] == 'record':
-            check_record(client, record=data_loaded['data'])
+            client.check_record(record=data_loaded['data'])
         elif data_loaded['type'] == 'set-id':
             client.set_data(data_loaded['data'])
-            client_server.socket.sendto(b'SUCCESS', addr)
+            client.accept_port.socket.sendto(b'SUCCESS', addr)
 
 
-def run_query(s, addr, client, long_name):
+def run_query(addr, client, long_name):
     '''
         Take in the query command and either return response with record found
         or call the next node with the same query command
@@ -154,17 +137,17 @@ def run_query(s, addr, client, long_name):
         records = client.local_hash_table[pos]
         for record in records:
             if record['Long Name'] == ' '.join(long_name):
-                s.send_response(addr, res='SUCCESS', type='query-result', data=record)
+                client.query_port.send_response(addr, res='SUCCESS', type='query-result', data=record)
                 return
         
-        s.send_response(addr, res='FAILURE', type='query-result')
+        client.query_port.send_response(addr, res='FAILURE', type='query-result')
     else:
         # This isn't the correct node for query
         client.query = ' '.join(long_name)
-        connect_query_nodes(s, client, addr)
+        connect_query_nodes(client, addr)
     
 
-def connect_query_nodes(s, client, origin, ip=None, port=None):
+def connect_query_nodes(client, origin, ip=None, port=None):
     '''
         Connect to given query address or the query address of the next node
     '''
@@ -172,7 +155,7 @@ def connect_query_nodes(s, client, origin, ip=None, port=None):
     if client.began_query and not ip:
         client.began_query = False
         print("Query looped through and didn't find record")
-        s.send_response(origin, res='FAILURE', type='query-result')
+        client.query_port.send_response(origin, res='FAILURE', type='query-result')
         
 
     if client.query:
@@ -189,10 +172,10 @@ def connect_query_nodes(s, client, origin, ip=None, port=None):
             if ip:
                 print(f'sending query info {data_loaded} to address {ip}, {port}')
                 client.began_query = True
-                s.socket.sendto(query, (ip, port))
+                client.query_port.socket.sendto(query, (ip, port))
             else:
                 print(f'sending query info {data_loaded} to address {client.next_node_query_addr}')
-                s.socket.sendto(query, client.next_node_query_addr)
+                client.query_port.socket.sendto(query, client.next_node_query_addr)
             # Sent query now listening for response from next node!
         except:
             print("client-node: sendall() error within query connection")
@@ -201,7 +184,7 @@ def connect_query_nodes(s, client, origin, ip=None, port=None):
         #     time.sleep(1)
     else:
         print("missing query be sure to put 'query {Long Name}' in your query command")
-        s.send_response(origin, res='FAILURE', type='query-result')
+        client.query_port.send_response(origin, res='FAILURE', type='query-result')
 
 
 def client_query_socket(client):
@@ -209,10 +192,9 @@ def client_query_socket(client):
         Set up the socket for query port which will accept all connections and then start
         a new listening thread
     '''
-    client_query = UDPServer()
 
     try:
-        client_query.socket.bind(client.query_addr)
+        client.query_port.socket.bind(client.query_addr)
     except Exception as error:
             print(error)
             print("query-server: bind() failed")
@@ -224,14 +206,14 @@ def client_query_socket(client):
     
     while True:
         
-        message = client_query.socket.recv(BUFFER_SIZE)
+        message = client.query_port.socket.recv(BUFFER_SIZE)
 
         # print(f"Query port received message: {message} from addr: {addr}")
 
-        client_query_conn(client_query, client, message)
+        client_query_conn(client, message)
 
 
-def client_query_conn(client_query, client, data):
+def client_query_conn(client, data):
     '''
         Socket connection listener that will listen for query commands
     '''
@@ -253,12 +235,12 @@ def client_query_conn(client_query, client, data):
         # print(f"query-conn: received message ``{data_list}''\n")
         if data_list[0] == 'query':
             addr = tuple(data_loaded['origin'])
-            run_query(client_query, addr, client, data_list[1:])
+            run_query(addr, client, data_list[1:])
         else:
             print(f"Incorrect data received at query port {data_loaded}")
 
 
-def connect_all_nodes(s, client):
+def connect_all_nodes(client):
     i = 1
 
     while i < len(client.user_dht):
@@ -268,8 +250,8 @@ def connect_all_nodes(s, client):
             data = (client.user_dht[i], client.user_dht[0])
         # print("Sending data: ", data)
         try:
-            s.send_response(addr=(client.user_dht[i]['ip'], int(client.user_dht[i]['port'])), res='SUCCESS', type='set-id', data=data)
-            s.socket.recv(BUFFER_SIZE)
+            client.send_port.send_response(addr=(client.user_dht[i]['ip'], int(client.user_dht[i]['port'])), res='SUCCESS', type='set-id', data=data)
+            client.send_port.socket.recv(BUFFER_SIZE)
             print(f"Successfully sent set-id command to {client.user_dht[i]['username']}")
         except Exception as error:
             print(error)
@@ -278,28 +260,12 @@ def connect_all_nodes(s, client):
         i += 1
 
 
-def connect_nodes(s, client):
-    '''
-        This function will connect the current node with the node to the right
-        in the DHT
-    '''
-    print("Successfully began loop to send records to next node!\n Awaiting records to forward\n\n")
-    while True:
-        if client.record:
-            try:
-                # print(f"sending to next node addr {client.next_node_addr}")
-                s.send_response(addr=client.next_node_addr, res='SUCCESS', type='record', data=client.record)
-            except:
-                print("client-node: sendall() error within records connect nodes")
-            client.record = None
-
-
-def listen(s, client):
+def listen(client):
     '''
         Listen function that will listen to all responses from the server connected to client
     '''
     while True:
-        data = s.socket.recv(BUFFER_SIZE)
+        data = client.client_to_server.socket.recv(BUFFER_SIZE)
         data_loaded = data.decode('utf-8')
 
         if data_loaded:
@@ -317,11 +283,11 @@ def listen(s, client):
             
             if data_loaded['type'] == 'DHT':
                 client.set_data(data_loaded['data'])
-                connect_all_nodes(s, client)
+                connect_all_nodes(client)
                 setup_all_local_dht(client)
                 success_string = bytes(f'dht-complete {client.username}', 'utf-8')
                 try:
-                    s.socket.sendto(success_string, client.server_addr)
+                    client.client_to_server.socket.sendto(success_string, client.server_addr)
                 except:
                     print("client: sendall() error sending success string")
                     return
@@ -331,15 +297,15 @@ def listen(s, client):
                 # print(f"Received query of {client.query}")
                 first_ip = data_loaded['data']['ip']
                 first_port = int(data_loaded['data']['query'])
-                connect_query_nodes(s, client, origin=client.query_addr, ip=first_ip, port=first_port)
+                connect_query_nodes(client, origin=client.query_addr, ip=first_ip, port=first_port)
                 # print(response)
         else:
             print(data_loaded)
 
 
 def main(args):
-    if not (len(args) == 6):
-        sys.exit(f"Usage: {args[0]} <Server IP address> <Server Port> <Client IP address> <Client Port> <Client Query Port>\n")
+    if not (len(args) == 7):
+        sys.exit(f"Usage: {args[0]} <Server IP address> <Server Port> <Client IP address> <Client Accept Port> <Client Query Port> <Client Send Port>\n")
     
     serv_IP = args[1]  # First arg: server IP address (dotted decimal)
     echo_serv_port = int(args[2])  # Second arg: Use given port
@@ -347,8 +313,10 @@ def main(args):
     client_port = int(args[4])
     query_ip = client_IP
     query_port = int(args[5])
+    right_ip = client_IP
+    right_port = int(args[6])
 
-    client = Client(serv_IP, echo_serv_port, client_IP, client_port, query_ip, query_port)
+    client = Client(serv_IP, echo_serv_port, client_IP, client_port, query_ip, query_port, right_ip, right_port)
 
     # Start the client server
     print('Starting client topology socket\n')
@@ -361,11 +329,10 @@ def main(args):
 
     # Start socket for connection to main server
     print(f"client: Arguments passed: server IP {client.server_addr}\n")
-    client_to_server = UDPServer()
     
 
     print("Starting thread to listen to server\n")
-    start_new_thread(listen, (client_to_server, client))
+    start_new_thread(listen, (client, ))
     
     while True:
         time.sleep(0.5)
@@ -375,7 +342,7 @@ def main(args):
             # print(f"\nClient: reads string ``{echo_string}''\n")
             echo_string = bytes(echo_string, 'utf-8')
             try:
-                client_to_server.socket.sendto(echo_string, client.server_addr)
+                client.client_to_server.socket.sendto(echo_string, client.server_addr)
                 
             except Exception as error:
                 print(error)
